fbbe1154abe271fdeead644dc91a734df0059bd4
Michael Svit <michaelsvit@gmail.com>
2017-08-11 13:38:51 +0300

    Restructure file and add documentation

--- game_engine.h
+++ game_engine.h
@@ -1,6 +1,9 @@
-/* Game engine module containing state and interaction related functions */
 #ifndef GAME_H
 #define GAME_H
+/*
+ * Game engine module containing state and interaction related functions
+ */
+#include "SPArrayList.h"
 
 #define BOARD_SIZE 8
 #define ARRAY_SIZE 16
@@ -8,8 +11,17 @@
 typedef enum {PLAYER1, PLAYER2} Player;
 typedef enum {PAWN, BISHOP, ROOK, KNIGHT, QUEEN, KING} PieceType;
 typedef enum {BLACK, WHITE} Color;
-typedef enum {1_PLAYER, 2_PLAYER} Mode;
+typedef enum {ONE_PLAYER, TWO_PLAYER} Mode;
 
+/* Struct defining a single game piece on the board */
+typedef struct game_piece{
+	PieceType type;
+	Color color;
+	int pos_y;
+	int pos_x;
+} GamePiece;
+
+/* Struct defining a board game with relevant properties */
 typedef struct game_t{
 	GamePiece *board[BOARD_SIZE][BOARD_SIZE]; /* current game board */
 	SPArrayList white_pieces, black_pieces; /* lists representing white and black game pieces on the board */
@@ -19,15 +31,17 @@ typedef struct game_t{
 	int difficulty;
 } Game;
 
-/* struct defining a single game piece on the board */
-typedef struct game_piece{
-	PieceType type;
-	Color color;
-	int pos_y;
-	int pos_x;
-} GamePiece;
-
+/*
+ * Creates a new game board.
+ * @param mode          	 one player or two player mode
+ * @param difficulty    	 chosen game difficulty, between 1 and 4
+ * @param player1_color 	 first player's game piece color, second player is the inverse color
+ * @return              	 pointer to the newly created game on success, NULL otherwise
+ */
 Game *create_game(Mode mode, int difficulty, Color player1_color);
 
+/*
+ * Destroy an existing game board.
+ */
 void destroy_game();
 #endif

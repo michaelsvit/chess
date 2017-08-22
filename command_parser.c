1362ca1e02506007ecf53287ec5ecaf43a08b5a0
Michael Svit <michaelsvit@gmail.com>
2017-08-22 16:14:21 +0300

    Implement command parser for game state

--- command_parser.c
+++ command_parser.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "command_parser.h"
 
 /******************************** Interface functions ******************************/
@@ -10,21 +11,44 @@ SettingCommand *parse_setting_command(const char *command_str){
 	strcpy(str_copy, command_str);
 
 	char *cmd_name, *cmd_arg;
-	const char *delimeter = " \t\r\n";
 
-	cmd_name = strtok(str_copy, delimeter);
+	cmd_name = strtok(str_copy, DELIMITER);
 	if(!cmd_name){
 		free(str_copy);
 		return NULL;
 	}
 
-	cmd_arg = strtok(NULL, delimeter);
+	cmd_arg = strtok(NULL, DELIMITER);
 	SettingCommand *cmd = get_settings_command(cmd_name, cmd_arg);
 
 	/* check no extra arguments are given */
-	cmd_arg = strtok(NULL, delimeter);
+	cmd_arg = strtok(NULL, DELIMITER);
 	if (cmd && cmd_arg != NULL) {
-		cmd->type = INVALID_COMMAND;
+		cmd->type = INVALID_SETTING_COMMAND;
+	}
+
+	free(str_copy);
+	return cmd;
+}
+
+GameCommand *parse_game_command(const char *command_str){
+	/* Create copy of given string to prevent segfault when running tests on string literal */
+	char *str_copy = (char *)malloc(strlen(command_str)+1);
+	strcpy(str_copy, command_str);
+
+	char *cmd_name;
+
+	cmd_name = strtok(str_copy, DELIMITER);
+	if(!cmd_name){
+		free(str_copy);
+		return NULL;
+	}
+
+	GameCommand *cmd = get_game_command(cmd_name);
+
+	/* check no extra arguments are given */
+	if (cmd && strtok(NULL, DELIMITER)) {
+		cmd->type = INVALID_GAME_COMMAND;
 	}
 
 	free(str_copy);
@@ -68,11 +92,39 @@ SettingCommand *get_settings_command(char *cmd_name, char *cmd_arg){
 	} else if (strcmp(cmd_name, "print_setting") == 0) {
 		cmd->type = PRINT_SETTING;
 	} else if (strcmp(cmd_name, "quit") == 0) {
-		cmd->type = QUIT;
+		cmd->type = SETTING_QUIT;
 	} else if (strcmp(cmd_name, "start") == 0) {
 		cmd->type = START;
 	} else {
-		cmd->type = INVALID_COMMAND;
+		cmd->type = INVALID_SETTING_COMMAND;
+	}
+	return cmd;
+}
+
+GameCommand *get_game_command(char *cmd_name){
+	GameCommand *cmd = (GameCommand *)malloc(sizeof(GameCommand));
+	if(!cmd) return NULL;
+	cmd->arg = NULL;
+
+	if(strcmp(cmd_name, "move") == 0){
+		cmd->type = MOVE;
+		cmd->arg = parse_move_args();
+	} else if (strcmp(cmd_name, "get_moves") == 0) {
+		cmd->type = GET_MOVES;
+		cmd->arg = parse_position();
+	} else if (strcmp(cmd_name, "save") == 0) {
+		cmd->type = SAVE;
+		char *arg = strtok(NULL, DELIMITER);
+		cmd->arg = malloc(strlen(arg)+1);
+		strcpy((char *)cmd->arg, arg);
+	} else if (strcmp(cmd_name, "undo") == 0) {
+		cmd->type = UNDO;
+	} else if (strcmp(cmd_name, "reset") == 0) {
+		cmd->type = RESET;
+	} else if (strcmp(cmd_name, "quit") == 0) {
+		cmd->type = GAME_QUIT;
+	} else {
+		cmd->type = INVALID_GAME_COMMAND;
 	}
 	return cmd;
 }
@@ -86,4 +138,41 @@ int is_valid_int(const char *str){
 		}
 	}
 	return 1;
+}
+
+int *parse_move_args(){
+	int *src = parse_position();
+	if(!src) return NULL;
+	/* Consume "to" word from command */
+	strtok(NULL, DELIMITER);
+	int *dst = parse_position();
+	if(!dst){
+		free(src);
+		return NULL;
+	}
+
+	int *args = (int *)malloc(sizeof(int) * 4);
+	args[0] = src[0];
+	args[1] = src[1];
+	args[2] = dst[0];
+	args[3] = dst[1];
+
+	free(src);
+	free(dst);
+	return args;
+}
+
+int *parse_position(){
+	char *arg = strtok(NULL, DELIMITER);
+	if(!arg) return NULL;
+
+	if(strlen(arg) != 5 || arg[0] != '<' || arg[2] != ',' || arg[4] != '>') return NULL;
+	if(!isdigit(arg[1]) || !isdigit(arg[3])) return NULL;
+
+	int *pos = (int *)malloc(sizeof(int) * 2);
+	if(!pos) return NULL;
+
+	pos[0] = arg[1] - '0';
+	pos[1] = arg[3] - '0';
+	return pos;
 }
--- command_parser.h
+++ command_parser.h
@@ -9,15 +9,17 @@
  * parse_game_command     	 parse given user command in game state
  */
 
+#define DELIMITER  " \t\r\n"
+
 typedef enum setting_command_type{
-	INVALID_COMMAND,
+	INVALID_SETTING_COMMAND,
 	GAME_MODE,
 	DIFFICULTY,
 	USER_COLOR,
 	LOAD,
 	DEFAULT,
 	PRINT_SETTING,
-	QUIT,
+	SETTING_QUIT,
 	START
 } SettingCommandType;
 
@@ -26,6 +28,21 @@ typedef struct setting_command{
 	void *arg;
 } SettingCommand;
 
+typedef enum game_command_type{
+	INVALID_GAME_COMMAND,
+	MOVE,
+	GET_MOVES,
+	SAVE,
+	UNDO,
+	RESET,
+	GAME_QUIT
+} GameCommandType;
+
+typedef struct game_command{
+	GameCommandType type;
+	void *arg;
+} GameCommand;
+
 /*
  * Parse command given by user in settings state.
  * @param command_str 	 string of user input
@@ -33,12 +50,19 @@ typedef struct setting_command{
  */
 SettingCommand *parse_setting_command(const char *command_str);
 
+/*
+ * Parse command given by user in game state.
+ * @param command_str 	 string of user input
+ * @return            	 pointer to parsed command on success, NULL otherwise
+ */
+GameCommand *parse_game_command(const char *command_str);
+
 /*******************************************************************************************/
 /**************** Auxiliary functions - should not be called outside this module ***********/
 /*******************************************************************************************/
 
 /*
- * Get command according to given name and argument.
+ * Get setting command according to given name and argument.
  * @param cmd_name 	 name of user command
  * @param cmd_arg  	 argument of user command
  * @return         	 pointer to parsed command on success, NULL otherwise
@@ -46,10 +70,29 @@ SettingCommand *parse_setting_command(const char *command_str);
 SettingCommand *get_settings_command(char *cmd_name, char *cmd_arg);
 
 /*
+ * Get game command according to given name and argument.
+ * @param cmd_name  	 name of user command
+ * @return          	 pointer to parsed command on success, NULL otherwise
+ */
+GameCommand *get_game_command(char *cmd_name);
+
+/*
  * Check if given string is a valid integer.
  * @param str 	 string to be checked
  * @return    	 true iff str represents a valid integer
  */
 int is_valid_int(const char *str);
+
+/*
+ * Parse arguments for a move command.
+ * @return 	 {src_x, src_y, dst_x, dst_y} on success, NULL otherwise
+ */
+int *parse_move_args();
+
+/*
+ * Parse position from argument string of the form <pos_y,pos_x> where x=column, y=row.
+ * @return 	 {pos_x, pos_y} on success, NULL otherwise
+ */
+int *parse_position();
 
 #endif /* ifndef COMMAND_PARSER_H */

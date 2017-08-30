2dfe9ce0f6323ca77308d6bf9491698c370a5367
Michael Svit <michaelsvit@gmail.com>
2017-08-30 11:25:23 +0300

    Prevent adding to removed pieces list if no piece was removed

--- src/game_engine.c
+++ src/game_engine.c
@@ -518,7 +518,7 @@ EngineMessage add_move_to_history(Game *game, int src_x, int src_y, int dst_x, i
 	if(!move) return MALLOC_FAILURE;
 	spArrayListAddFirst(game->move_history, move);
 	GamePiece *piece = game->board[dst_y][dst_x];
-	spArrayListAddLast(game->removed_pieces, piece);
+	if(piece) spArrayListAddLast(game->removed_pieces, piece);
 	return SUCCESS;
 }
 
